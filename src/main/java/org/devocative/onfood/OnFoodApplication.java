990981b13ee388ec826db4fd231f6ce6f31554d7
mbizhani <mbizhani@gmail.com>
2021-09-28 17:46:49 +0330

    + enable liquibase & its maven plugin

--- src/main/java/org/devocative/onfood/OnFoodApplication.java
+++ src/main/java/org/devocative/onfood/OnFoodApplication.java
@@ -2,6 +2,7 @@ package org.devocative.onfood;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.domain.AuditorAware;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
@@ -11,7 +12,8 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import java.util.Optional;
 
 @EnableJpaAuditing(auditorAwareRef = "auditorAware")
-@SpringBootApplication
+// TIP: for exclude, https://stackoverflow.com/questions/30761253/remove-using-default-security-password-on-spring-boot
+@SpringBootApplication(exclude = {UserDetailsServiceAutoConfiguration.class})
 public class OnFoodApplication {
 
 	@Bean
